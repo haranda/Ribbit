0354a2256261ebbeff4d65f30365669d0e103a9d
Hernan Aranda <hernan.aranda.gomez@gmail.com>
2014-03-16 15:33:54 -0400

    Login & Logout implemented

--- Ribbit/src/com/teamtreehouse/ribbit/RibbitApplication.java
+++ Ribbit/src/com/teamtreehouse/ribbit/RibbitApplication.java
@@ -3,18 +3,12 @@ package com.teamtreehouse.ribbit;
 import android.app.Application;
 
 import com.parse.Parse;
-import com.parse.ParseObject;
 
 public class RibbitApplication extends Application {
 	
 	@Override
 	public void onCreate() { 
 		super.onCreate();
-		
-		Parse.initialize(this, "rjgTQR4N7uMU0RvB5aixLLnTv6XyBErXwPkiuj0K", "xfuNv0t0AzcATbmE3pBJUdrnFEcsVUc7FBRgACMb");
-	    
-	    ParseObject testObject = new ParseObject("TestObject");
-	    testObject.put("foo", "bar");
-	    testObject.saveInBackground();
+	    Parse.initialize(this, "rjgTQR4N7uMU0RvB5aixLLnTv6XyBErXwPkiuj0K", "xfuNv0t0AzcATbmE3pBJUdrnFEcsVUc7FBRgACMb");
 	}
 }
