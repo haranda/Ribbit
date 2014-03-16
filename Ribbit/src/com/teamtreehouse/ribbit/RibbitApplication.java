83d92fcaefb673e82ea04a4ecfff5f122e190320
Hernan Aranda <hernan.aranda.gomez@gmail.com>
2014-03-16 13:58:56 -0400

    Changed Parse.initialize from the onCreate method of your Application class to set my application id and client key.

--- Ribbit/src/com/teamtreehouse/ribbit/RibbitApplication.java
+++ Ribbit/src/com/teamtreehouse/ribbit/RibbitApplication.java
@@ -10,7 +10,8 @@ public class RibbitApplication extends Application {
 	@Override
 	public void onCreate() { 
 		super.onCreate();
-	    Parse.initialize(this, "dbrOyEHgX2GR3EPTIblrNNAd4ITXarc82shcXdlu", "6iyrGf1I8pXKRGlxMuL4NQQ4doZLrR5btJ2ZlvII");
+		
+		Parse.initialize(this, "rjgTQR4N7uMU0RvB5aixLLnTv6XyBErXwPkiuj0K", "xfuNv0t0AzcATbmE3pBJUdrnFEcsVUc7FBRgACMb");
 	    
 	    ParseObject testObject = new ParseObject("TestObject");
 	    testObject.put("foo", "bar");
