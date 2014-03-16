8955decaeaebb6edda72239fd6ad6800580e7726
Hernan Aranda <hernan.aranda.gomez@gmail.com>
2014-03-16 17:13:13 -0400

    Fragments for Inbox and Friend added

--- Ribbit/src/com/teamtreehouse/ribbit/RibbitApplication.java
+++ Ribbit/src/com/teamtreehouse/ribbit/RibbitApplication.java
@@ -9,6 +9,6 @@ public class RibbitApplication extends Application {
 	@Override
 	public void onCreate() { 
 		super.onCreate();
-	    Parse.initialize(this, "rjgTQR4N7uMU0RvB5aixLLnTv6XyBErXwPkiuj0K", "xfuNv0t0AzcATbmE3pBJUdrnFEcsVUc7FBRgACMb");
+		Parse.initialize(this, "rjgTQR4N7uMU0RvB5aixLLnTv6XyBErXwPkiuj0K", "xfuNv0t0AzcATbmE3pBJUdrnFEcsVUc7FBRgACMb");
 	}
 }
